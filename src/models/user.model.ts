ff79c852fa229c6259f575fc568fd32acd4ec763
Ricky <rickyandrianaivo@gmail.com>
2024-11-24 09:08:51 +0300

    client-profile page

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,19 +2,50 @@ import { Schema, model } from "mongoose";
 
 export interface User{
     // id              : string,
-    email           : string,
-    password        : string,
-    name            : string,
-    phoneNumber     : string;
-    securityLevel   : string;
+    userName            : string;
+    userFirstname       :string;
+    userPassword        : string;
+    userEmail           : string;
+    userPhone           : string;
+    userDescritpion     : string;
+    userGender          : string;
+    userImage           : string;
+    userEnabled         : boolean;
+    userDateOfBirth     : Date;
+    userTotalSolde      : number;
+    userLogo            : string;
+    userStatut          : string;
+    userManager         : string;
+    userNif             : string;
+    userRC              : string;
+    identityDocumentType: string;
+    identityCardNumber  : string;
+    userAdmin           : boolean;
+    userAddress         : string;
+    userIdentityCode    : string;
 }
 
 export const UserSchema = new Schema<User>({
-    email           : {type : String},
-    password        : {type : String, required : true},
-    name            : {type : String, required : true},
-    phoneNumber     : {type : String, required : true},
-    securityLevel   : {type : String, required : true},
+    userName                : {type : String},
+    userFirstname           : {type : String, required : true},
+    userPassword            : {type : String, required : true},
+    userEmail               : {type : String, required : true},
+    userPhone               : {type : String, required : true},
+    userDescritpion         : {type : String},
+    userGender              : {type : String, required : true},
+    userImage               : {type : String},
+    userEnabled             : {type : Boolean, required : true},
+    userDateOfBirth         : {type : Date, required : true},
+    userTotalSolde          : {type : Number},
+    userLogo                : {type : String},
+    userStatut              : {type : String},
+    userManager             : {type : String},
+    userNif                 : {type : String},
+    userRC                  : {type : String},
+    identityDocumentType    : {type : String, required : true},
+    identityCardNumber      : {type : String, required : true},
+    userAddress             : {type : String},
+    userIdentityCode        : {type : String},
 },{
     timestamps : true,
     toJSON : {
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -3,68 +3,116 @@ import asyncHandler from "express-async-handler";
 import { User, UserModel } from "../models/user.model";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
-import { FactureVenteModel } from '../models/factureVente.model';
-import { BonEntreeModel } from '../models/bonEntree.model';
-import { BonSortiesModel } from '../models/bonSortie.model';
-import { BonEntreeDetailsModel, BonEntreeDetailsSchema } from '../models/bonEntreeDetails.model';
-import { BonSortiesDetailsModel } from '../models/bonSortieDetails.model';
-import { InventaireDetailModel } from '../models/inventaireDetail.model';
-import { InventaireModel } from '../models/inventaire.model';
-import { FactureVenteDetailsModel } from '../models/factureVenteDetails.model';
-import { MouvementStockModel } from '../models/mouvementStock.model';
-import { PointDeVenteModel } from '../models/pointDeVente.model';
 
 const router = Router();
 
 router.post("/register/",asyncHandler(async(req, res) => {
 
-    const {name,email,password,phoneNumber,securityLevel} = req.body;
-    const user = await UserModel.findOne({name : name});
+    const {
+        userName,
+        userFirstname,
+        userPassword,
+        userEmail,
+        userPhone,
+        userDescritpion,
+        userGender,
+        userImage,
+        userEnabled,
+        userDateOfBirth,
+        userTotalSolde,
+        userLogo,
+        userStatut,
+        userManager,
+        userNif ,
+        userRC ,
+        identityDocumentType,
+        identityCardNumber,
+        userAdmin,
+        userAddress ,
+        userIdentityCode,} = req.body;
+    const user = await UserModel.findOne({userName : userName});
 
     if(user){
         res.send("Ce nom est déjà utilisé!");
         return;
     }
 
-    const encryptedPassword = await bcrypt.hash(password,10);
+    const encryptedPassword = await bcrypt.hash(userPassword,10);
     const newUser:User = {
-        name,
-        email:email.toLowerCase(),
-        password: encryptedPassword,
-        phoneNumber,
-        securityLevel,
+        userName,
+        userFirstname,
+        userPassword: encryptedPassword,
+        userEmail:userEmail.toLowerCase(),
+        userPhone,
+        userDescritpion,
+        userGender,
+        userImage,
+        userEnabled,
+        userDateOfBirth,
+        userTotalSolde,
+        userLogo,
+        userStatut,
+        userManager,
+        userNif ,
+        userRC ,
+        identityDocumentType,
+        identityCardNumber,
+        userAdmin,
+        userAddress ,
+        userIdentityCode,
     }
-
     const dbUser = await UserModel.create(newUser);
-    res.send(generateTokenResponse(dbUser));
+    // res.send(generateTokenResponse(dbUser));
 }))
 
 const generateTokenResponse = (user:any) =>{
     const token = jwt.sign({
-        id: user.id, email:user.email,name: user.name,phoneNumber:user.phoneNumber, securityLevel:user.securityLevel
+        _id: user._id,
+        userEmail:user.userEmail,
+        userName: user.userName,
+        userFirstname:user.userFirstname,
+        userPhone:user.userPhone,
     },process.env.JWT_SECRET!,{
         expiresIn:"30d"
     });
     return {
-        id              : user.id,
-        email           : user.email,
-        name            : user.name,
-        phoneNumber     : user.phoneNumber,
-        securityLevel   : user.securityLevel,
+        _id             : user._id,
+        userEmail       : user.userEmail,
+        userName        : user.userName,
+        userFirstname   : user.userFirstname,
+        userPhone       : user.userPhone,
+        userDescritpion : user.userDescription,
+        userGender      : user.userGender,
+        userTotalSolde  : user.userTotalSolde,
+        userStatut      : user.userStatut,
+        userManager     : user.userManager,
+        userNif         : user.userNif,
+        userRC          : user.userRC ,
         token           : token
     };
 }
 
-router.get("/", asyncHandler(async(req, res) => {
+router.get("", asyncHandler(async(req, res) => {
     const users = await UserModel.find();
     res.send(users);
 }))
+router.get("/:id", asyncHandler(async(req, res) => {
+    const userId = req.params['id'];
+    const user = await UserModel.findOne({_id : userId});
+    res.send(user);
+}))
+router.get("/email/:email", asyncHandler(async(req, res) => {
+    const userEmail = req.params['email'];
+    const user = await UserModel.findOne({userEmail : userEmail});
+    res.send(user);
+}))
 
 router.post("/login",asyncHandler(async(req,res) => {
-    const {name,password} = req.body;
-    const user = await UserModel.findOne({name})
-    if (user && (await bcrypt.compare(password,user.password))) {
+    const {userEmail,userPassword} = req.body;
+    const user = await UserModel.findOne({userEmail})
+    if (user && (await bcrypt.compare(userPassword,user.userPassword))) {
         res.send(generateTokenResponse(user));
+        
     }else{
         res.status(404).send("User name or password is not valid!")
     }
@@ -72,54 +120,98 @@ router.post("/login",asyncHandler(async(req,res) => {
 
 router.put("/update/:id",asyncHandler(async(req,res) => {
     const userId = req.params['id'];
-    const {name,password,email,phoneNumber,securityLevel} = req.body;
-    const userUpdates = {name,password,email,phoneNumber,securityLevel};
+    const {
+        userName,
+        userFirstname,
+        userPassword,
+        userEmail,
+        userPhone,
+        userDescritpion,
+        userGender,
+        userImage,
+        userEnabled,
+        userDateOfBirth,
+        userTotalSolde,
+        userLogo,
+        userStatut,
+        userManager,
+        userNif ,
+        userRC ,
+        identityDocumentType,
+        identityCardNumber,
+        userAdmin,
+        userAddress ,
+        userIdentityCode,
+    } = req.body;
+    const userUpdates = {
+        userName,
+        userFirstname,
+        userPassword,
+        userEmail:userEmail.toLowerCase(),
+        userPhone,
+        userDescritpion,
+        userGender,
+        userImage,
+        userEnabled,
+        userDateOfBirth,
+        userTotalSolde,
+        userLogo,
+        userStatut,
+        userManager,
+        userNif ,
+        userRC ,
+        identityDocumentType,
+        identityCardNumber,
+        userAdmin,
+        userAddress ,
+        userIdentityCode,
+        };
 
     await UserModel.updateOne({id : userId}, userUpdates);
 }))
 
-router.get("/resetTable",asyncHandler(async(req,res)=>{
-    const factureVenteCount = await FactureVenteModel.countDocuments()
-    const factureVenteDetailsCount = await FactureVenteDetailsModel.countDocuments()
-    const bonEntreeCount = await BonEntreeModel.countDocuments()
-    const bonEntreeDetailsCount = await BonEntreeDetailsModel.countDocuments()
-    const bonSortiesCount = await BonSortiesModel.countDocuments()
-    const bonSortiesDetailsCount = await BonSortiesDetailsModel.countDocuments()
-    const inventaireDetailCount = await InventaireDetailModel.countDocuments()
-    const inventaireCount = await InventaireModel.countDocuments()
-    const mouvementStockCount = await MouvementStockModel.countDocuments()
-    if (factureVenteCount > 0 ||
-        factureVenteDetailsCount >0 ||
-        bonEntreeCount > 0 || 
-        bonEntreeDetailsCount > 0||
-        bonSortiesCount > 0 ||
-        bonSortiesDetailsCount > 0||
-        inventaireDetailCount > 0||
-        inventaireCount > 0 ||
-        mouvementStockCount > 0) 
-    {
-        await FactureVenteModel.deleteMany({})
-        await FactureVenteDetailsModel.deleteMany({})
-        await BonEntreeModel.deleteMany({})
-        await BonEntreeDetailsModel.deleteMany({})
-        await BonSortiesModel.deleteMany({})
-        await BonSortiesDetailsModel.deleteMany({})
-        await InventaireDetailModel.deleteMany({})
-        await InventaireModel.deleteMany({})
-        await MouvementStockModel.deleteMany({})
-        await PointDeVenteModel.updateOne({},{
-            $set : {
-                numeroBE:1,
-                numeroBS:1,
-                numeroInventaire:1,
-                numeroMouvementStock:1,
-                numeroVente:1
-            }})
-        res.status(200).send("reset is done !")
-    }else{
-        res.send("No item to delete")
-    }
+// router.get("/resetTable",asyncHandler(async(req,res)=>{
+//     const factureVenteCount = await FactureVenteModel.countDocuments()
+//     const factureVenteDetailsCount = await FactureVenteDetailsModel.countDocuments()
+//     const bonEntreeCount = await BonEntreeModel.countDocuments()
+//     const bonEntreeDetailsCount = await BonEntreeDetailsModel.countDocuments()
+//     const bonSortiesCount = await BonSortiesModel.countDocuments()
+//     const bonSortiesDetailsCount = await BonSortiesDetailsModel.countDocuments()
+//     const inventaireDetailCount = await InventaireDetailModel.countDocuments()
+//     const inventaireCount = await InventaireModel.countDocuments()
+//     const mouvementStockCount = await MouvementStockModel.countDocuments()
+//     if (factureVenteCount > 0 ||
+//         factureVenteDetailsCount >0 ||
+//         bonEntreeCount > 0 || 
+//         bonEntreeDetailsCount > 0||
+//         bonSortiesCount > 0 ||
+//         bonSortiesDetailsCount > 0||
+//         inventaireDetailCount > 0||
+//         inventaireCount > 0 ||
+//         mouvementStockCount > 0) 
+//     {
+//         await FactureVenteModel.deleteMany({})
+//         await FactureVenteDetailsModel.deleteMany({})
+//         await BonEntreeModel.deleteMany({})
+//         await BonEntreeDetailsModel.deleteMany({})
+//         await BonSortiesModel.deleteMany({})
+//         await BonSortiesDetailsModel.deleteMany({})
+//         await InventaireDetailModel.deleteMany({})
+//         await InventaireModel.deleteMany({})
+//         await MouvementStockModel.deleteMany({})
+//         await PointDeVenteModel.updateOne({},{
+//             $set : {
+//                 numeroBE:1,
+//                 numeroBS:1,
+//                 numeroInventaire:1,
+//                 numeroMouvementStock:1,
+//                 numeroVente:1
+//             }})
+//         res.status(200).send("reset is done !")
+//     }else{
+//         res.send("No item to delete")
+//     }
     
-}))
+// }))
 
 export default router;
