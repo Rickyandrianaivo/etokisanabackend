aacfff3b1391abe7fb74027f6650c3ea4edbfd91
Ricky <rickyandrianaivo@gmail.com>
2025-01-12 22:01:32 +0300

    creationUsertogetID

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -77,7 +77,8 @@ router.post("/register/",asyncHandler(async(req, res) => {
             userAddress ,
             userIdentityCode,
         }
-        tokenInfo = generateTokenResponse(newUser);
+        const userDb = await UserModel.create(newUser);
+        tokenInfo = generateTokenResponse(userDb);
         const tokenDB = {
           id    : tokenInfo._id,
           token : tokenInfo.token
