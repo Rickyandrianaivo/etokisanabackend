84f05a90af207486929ba277cdc5951018b4cf17
Ricky <rickyandrianaivo@gmail.com>
2025-02-25 07:11:01 +0300

    images serving

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ dbConnect();
 const app = express();
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb',extended : true}));
+app.use('/uploads',express.static('uploads'));
 
 app.use(cors({
     // credentials:true,
