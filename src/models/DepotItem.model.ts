c6cd002af0639cd8dabb3a7c5d6ad82a90d02047
Ricky <rickyandrianaivo@gmail.com>
2025-08-06 16:36:11 +0300

    depotItemModel

---
+++ src/models/DepotItem.model.ts
@@ -0,0 +1,26 @@
+import { model, Schema } from "mongoose";
+
+export interface IDepotItem{
+    productId : string,
+    stock: number,
+    prix   : number,
+    lastUpdate : Date,
+    currentDepotId : string,
+}
+export const DepotItemSchema = new Schema<IDepotItem>({
+    productId : {type : String},
+    stock: {type : Number},
+    prix   : {type : Number},
+    lastUpdate : {type : Date},
+    currentDepotId : {type : String},
+},{
+    timestamps : true,
+    toJSON : {
+        virtuals : true
+    },
+    toObject : {
+        virtuals : true
+    }
+});
+
+export const CategoryModel = model<IDepotItem>('depotItem',DepotItemSchema)
