a80f07acc1b8dbac4430cd123ffab5e519ebdee1
Ricky <rickyandrianaivo@gmail.com>
2025-08-30 18:32:03 +0300

    no populate test

--- src/routers/depotItem.router.ts
+++ src/routers/depotItem.router.ts
@@ -114,10 +114,10 @@ router.patch('/modifyDepotItem/:id',expressAsyncHandler(async(req,res)=>{
 }))
 
 router.get('/ByProductId/:id',expressAsyncHandler(async(req,res)=>{
-    const allDepotItemByProductId = await DepotItemModel.find({productId : req.params['id']})
-    .populate('productId')
-    .populate('currentDepotId')
-    .exec();
+    const allDepotItemByProductId = await DepotItemModel.findOne({productId : req.params['id']})
+    // .populate('productId')
+    // .populate('currentDepotId')
+    // .exec();
     
     res.status(200).send(allDepotItemByProductId);
 }))
